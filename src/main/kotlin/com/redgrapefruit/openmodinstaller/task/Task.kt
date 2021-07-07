9c13c205bf2284772e3c3982659251337a911005
RedGrapefruit <karpovanton729@gmail.com>
2021-07-07 17:24:08 +0300

    Improve task system

--- src/main/kotlin/com/redgrapefruit/openmodinstaller/task/Task.kt
+++ src/main/kotlin/com/redgrapefruit/openmodinstaller/task/Task.kt
@@ -3,10 +3,14 @@ package com.redgrapefruit.openmodinstaller.task
 /**
  * A task can be executed and has a [TaskContext] provided
  */
-interface Task {
-    fun preLaunch(context: PreLaunchTaskContext) = Unit
-    fun launch(context: LaunchTaskContext)
-    fun postLaunch(context: PostLaunchTaskContext) = Unit
+interface Task<
+        TPreLaunchContext : PreLaunchTaskContext,
+        TLaunchContext : LaunchTaskContext,
+        TPostLaunchContext : PostLaunchTaskContext> {
+
+    fun preLaunch(context: TPreLaunchContext) = Unit
+    fun launch(context: TLaunchContext)
+    fun postLaunch(context: TPostLaunchContext) = Unit
 }
 
 // Contexts
@@ -16,3 +20,8 @@ interface PreLaunchTaskContext : TaskContext
 interface LaunchTaskContext : TaskContext
 interface PostLaunchTaskContext : TaskContext
 
+// Default context impl-s where you don't use a context for an event
+
+object DefaultPreLaunchTaskContext : PreLaunchTaskContext
+object DefaultLaunchTaskContext : LaunchTaskContext
+object DefaultPostLaunchTaskContext : PostLaunchTaskContext
