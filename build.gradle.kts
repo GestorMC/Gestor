a3b6d6f6385d6d8bb07652e8674e23e3f4ca1114
RedGrapefruit <karpovanton729@gmail.com>
2021-07-10 10:53:54 +0300

    Import Mojang Authlib into buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,16 +14,23 @@ version = "1.0"
 repositories {
     mavenCentral()
     maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+
+    // libraries.minecraft.net has Authlib
+    maven { url = uri("https://libraries.minecraft.net/") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation(compose.desktop.currentOs)
+    // KotlinX Serialization. JSON support
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    // CommonMark3. Markdown -> HTML rendering
     implementation("org.commonmark:commonmark:0.18.0")
+    // Mojang Authlib. Verification of accounts
+    implementation("com.mojang:authlib:2.3.31")
+    // Miscellaneous utilities
     implementation(group = "com.google.code.gson", name = "gson", version = "2.8.7")
     implementation(group = "org.apache.commons", name = "commons-lang3", version = "3.12.0")
-
 }
 
 tasks.test {
