a6f82d26ae3e46e06b328817eaee85116129540f
lucsoft <mail@lucsoft.de>
2021-07-11 19:11:26 +0200

    Fixed code not running async

--- src/main/kotlin/com/redgrapefruit/openmodinstaller/task/ProvideImage.kt
+++ src/main/kotlin/com/redgrapefruit/openmodinstaller/task/ProvideImage.kt
@@ -3,16 +3,22 @@ package com.redgrapefruit.openmodinstaller.task
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import com.redgrapefruit.openmodinstaller.consts.assetCache
+import kotlinx.coroutines.*
 import okhttp3.*
+import java.util.concurrent.TimeUnit
 
 class Event(val url: String): EventListener() {
+    override fun cacheHit(call: Call, response: Response) {
+        super.cacheHit(call, response)
+        println("From Cache: $url")
+    }
     override fun cacheMiss(call: Call) {
         super.cacheMiss(call)
         println("Loading Asset: $url")
     }
 }
 
-suspend fun DownloadFile(url: String): ResponseBody? {
+fun DownloadFile(url: String): ResponseBody? {
     val client: OkHttpClient = OkHttpClient.Builder().cache(assetCache).eventListener(Event(url)).build()
 
     val request = Request.Builder().url(url).build()
@@ -22,14 +28,18 @@ suspend fun DownloadFile(url: String): ResponseBody? {
 var cache = mutableMapOf<String, ImageBitmap>()
 
 fun getCachedBitmap(url: String): ImageBitmap? = cache[url]
+
+@OptIn(DelicateCoroutinesApi::class)
 suspend fun getBitmapFromURL(url: String): ImageBitmap {
     if (cache.contains(url))
         return cache[url]!!
 
-    val data = org.jetbrains.skija.Image.makeFromEncoded(
-        DownloadFile(url)!!.bytes()
-    ).asImageBitmap()
+    val data = GlobalScope.async {
+        return@async org.jetbrains.skija.Image.makeFromEncoded(
+            DownloadFile(url)!!.bytes()
+        ).asImageBitmap()
+    }
 
-    cache[url] = data;
-    return data
+    cache[url] = data.await()
+    return cache[url]!!
 }
