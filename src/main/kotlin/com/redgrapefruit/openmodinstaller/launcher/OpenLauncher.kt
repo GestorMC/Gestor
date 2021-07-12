57e85fe9d4b786d10451f226f2a814b02b876633
RedGrapefruit <karpovanton729@gmail.com>
2021-07-12 11:43:22 +0300

    Add basic gen of legacy MC arguments

--- src/main/kotlin/com/redgrapefruit/openmodinstaller/launcher/OpenLauncher.kt
+++ src/main/kotlin/com/redgrapefruit/openmodinstaller/launcher/OpenLauncher.kt
@@ -1,7 +1,13 @@
 package com.redgrapefruit.openmodinstaller.launcher
 
+import com.mcgoodtime.gjmlc.core.JavaArgumentHack
+import com.mojang.authlib.UserType
 import com.sun.security.auth.module.NTSystem
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import java.io.File
+import java.io.FileInputStream
 
 /**
  * The main class for launching the game
@@ -26,6 +32,10 @@ class OpenLauncher private constructor(private val root: String) {
         SetupManager.setupLibraries(root, version)
         SetupManager.setupJAR(root, version)
         SetupManager.setupJava(optInLegacyJava)
+
+        // Make dirs
+        File("$root/assets").mkdirs()
+
     }
 
     /**
@@ -38,9 +48,42 @@ class OpenLauncher private constructor(private val root: String) {
         rootFile.mkdir()
     }
 
-    fun launch(username: String, maxMemory: Int, jvmArgs: String = "") {
+    fun launch(username: String, maxMemory: Int, jvmArgs: String = "", version: String) {
+        val versionInfoPath = "$root/versions/$version/$version.json"
 
+        // Make sure version info is set up
+        if (!File(versionInfoPath).exists()) SetupManager.setupVersionInfo(root, version)
+
+        // Parse version info
+        val versionInfoObject: JsonObject
+        FileInputStream(versionInfoPath).use { stream ->
+            versionInfoObject = Json.decodeFromString(JsonObject.serializer(), stream.readBytes().decodeToString())
+        }
+
+        // Check if the legacy argument format is used
+        val arguments = if (versionInfoObject.contains("minecraftArguments")) {
+            generateLegacyArguments(
+                raw = versionInfoObject["minecraftArguments"]!!.jsonPrimitive.content,
+                version = version,
+                assetsIndexName = versionInfoObject["assets"]!!.jsonPrimitive.content)
+        } else {
+            generateModernArguments()
+        }
     }
+
+    private fun generateLegacyArguments(raw: String, version: String, assetsIndexName: String): String {
+        return raw
+            .replace(JavaArgumentHack.VERSION_NAME, version)
+            .replace(JavaArgumentHack.GAME_DIRECTORY, root)
+            .replace(JavaArgumentHack.ASSETS_ROOT, "$root/assets")
+            .replace(JavaArgumentHack.ASSETS_INDEX_NAME, assetsIndexName)
+            .replace(JavaArgumentHack.AUTH_UUID, "auth_uuid")
+            .replace(JavaArgumentHack.AUTH_ACCESS_TOKEN, "auth_access_token")
+            .replace(JavaArgumentHack.USER_PROPERTIES, "{}")
+            .replace(JavaArgumentHack.USER_TYPE, "legacy")
+    }
+
+    private fun generateModernArguments(): String = ""
 
     companion object {
         /**
@@ -50,8 +93,8 @@ class OpenLauncher private constructor(private val root: String) {
             /**
              * Game's root folder. Win AppData by default
              */
-            root: String = "C:/Users/${NTSystem().name}/AppData/Roaming/.minecraft"): OpenLauncher {
-            return OpenLauncher(root)
-        }
+            root: String = "C:/Users/${NTSystem().name}/AppData/Roaming/.minecraft")
+
+        : OpenLauncher = OpenLauncher(root)
     }
 }
