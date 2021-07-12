8c55c299512e1ec538f65c61e4288dfa595f2c1e
RedGrapefruit <karpovanton729@gmail.com>
2021-07-12 10:44:48 +0300

    Increase max cache size to 5GB from 1GB

--- src/main/kotlin/com/redgrapefruit/openmodinstaller/consts/cache.kt
+++ src/main/kotlin/com/redgrapefruit/openmodinstaller/consts/cache.kt
@@ -3,7 +3,7 @@ package com.redgrapefruit.openmodinstaller.consts
 import okhttp3.Cache
 import java.io.File
 
-const val CACHE_MAX_SIZE: Long = 1000L * 1024L * 1024L // 1GB max for heavy images
+const val CACHE_MAX_SIZE: Long = 5000L * 1024L * 1024L // 5GB max for heavy data, Java ZIPs and TAR.GZs etc.
 
 val assetCache = Cache(
     directory = File("./cache/"),
