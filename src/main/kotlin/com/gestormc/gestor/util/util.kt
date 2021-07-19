2856b6d1f0723d66341a555cdc5372c50df537fa
RedGrapefruit <karpovanton729@gmail.com>
2021-07-19 08:33:01 +0300

    Remove deprecated APIs

--- src/main/kotlin/com/gestormc/gestor/util/util.kt
+++ src/main/kotlin/com/gestormc/gestor/util/util.kt
@@ -2,34 +2,4 @@
 
 package com.gestormc.gestor.util
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import com.gestormc.gestor.core.ModInstaller
-import org.jetbrains.skija.Image
-import java.io.File
-import java.io.FileInputStream
-import kotlin.random.Random
-
-@Composable
-fun BitmapFromImageURL(url: String): ImageBitmap {
-    val imageFolder = "${File(Settings.cacheFolderField).parent}/images/"
-
-    val imageFolderFile = File(imageFolder)
-    if (imageFolderFile.exists()) imageFolderFile.mkdirs()
-
-    // TODO: Make a JSON (image_codecs.json) that will contain the cache number -> URL map to avoid unnecessary downloading
-    val path = "$imageFolder/img_${Random.nextInt(Int.MAX_VALUE)}"
-    File(path).createNewFile()
-
-    ModInstaller.downloadFile(url, path)
-
-    val raw: ByteArray
-    FileInputStream(path).use { stream ->
-        raw = stream.readBytes()
-    }
-
-    return Image.makeFromEncoded(raw).asImageBitmap()
-}
-
 operator fun StringBuilder.plusAssign(assign: String) { append(assign) }
