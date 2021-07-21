5fe13880f63582b3f4fe85779b6287ac0ecce029
RedGrapefruit <karpovanton729@gmail.com>
2021-07-21 15:50:21 +0300

    Small Unix fix

--- src/main/kotlin/com/gestormc/gestor/launcher/GestorLauncher.kt
+++ src/main/kotlin/com/gestormc/gestor/launcher/GestorLauncher.kt
@@ -10,6 +10,7 @@ import com.sun.security.auth.module.NTSystem
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import org.apache.commons.lang3.SystemUtils
 import java.io.*
 
 /**
@@ -432,8 +433,15 @@ class GestorLauncher private constructor(
             }
             if (subroot == null) throw RuntimeException("Couldn't find the subroot of the Java installation. The root is empty")
 
+            var out = "${subroot!!.absolutePath}/bin/java.exe"
+
+            if (SystemUtils.IS_OS_UNIX) {
+                // a small issue with the paths, it's /home/exampleuser/gestor/./java instead of /home/exampleuser/gestor/java
+                out = out.replace("./", "")
+            }
+
             // Return the main Java executable in the binaries folder
-            return "${subroot!!.absolutePath}/bin/java.exe"
+            return out
         }
 
         /**
